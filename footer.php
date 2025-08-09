c0f29500a1082e4372a731bd41706716bed8f8ea
Prottoy Kundu <163542890+prottoykundu38@users.noreply.github.com>
2025-08-09 21:44:34 +0600

    Adding footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Phyra - Student Mental Health Support Platform. All rights reserved.</p>
+    </div>
+</footer>
